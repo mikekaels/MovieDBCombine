86e737461059e93ae8ec436fa5af698a0d6ab8ef
MIke <santo.michael@privyid.tech>
2024-02-07 23:15:11 +0700

    change api key

---
+++ MovieDBCombine/APIConfig/APIKeys.swift
@@ -0,0 +1,14 @@
+//
+//  APIKeys.swift
+//  MovieDBCombine
+//
+//  Created by Santo Michael on 03/02/24.
+//
+
+import Foundation
+
+internal enum APIKeys {
+	static var theMovieDB: String {
+		return "dabd891097ebc46c5b0cab7182d05e30"
+	}
+}
